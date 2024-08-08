10e21676a9b097387a921d8946d5a11e3e4027d4
Miguel <maarcilac@eafit.edu.co>
2024-08-08 10:28:06 -0500

    visualizacion de peliculas con estilo

--- movie/views.py
+++ movie/views.py
@@ -5,13 +5,11 @@ from .models import Movie
 # Create your views here.
 
 def home(request):
-    
     searchTerm = request.GET.get('searchMovie')
     if searchTerm:
         movies = Movie.objects.filter(title__icontains=searchTerm)
     else:
         movies = Movie.objects.all()
-
     return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
 
 
